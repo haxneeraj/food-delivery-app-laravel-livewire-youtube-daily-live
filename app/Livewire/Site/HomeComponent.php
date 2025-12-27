5d4292d5d3da5ec176d797b0d99b6cdc5373911a
Neeraj Saini <hax-neeraj@outlook.com>
2025-12-27 22:05:41 +0530

    --init

---
+++ app/Livewire/Site/HomeComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Site;
+
+use Livewire\Component;
+
+class HomeComponent extends Component
+{
+    public function render()
+    {
+        return view('site.home-component');
+    }
+}
